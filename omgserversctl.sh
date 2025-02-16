e727b0cb7a83d995d989a2e0477b49955af0fe94
Kirill Byvshev <k@byv.sh>
2025-02-17 00:18:49 +0400

    Setting OMG_FORMATTING default value

--- omgserversctl.sh
+++ omgserversctl.sh
@@ -5,6 +5,7 @@ export TZ=UTC
 
 docker run --rm -it \
     --network=host \
+    -e OMG_FORMATTING=${OMG_FORMATTING:-true} \
     -v ${PWD}/.omgserversctl:/opt/omgservers/.omgserversctl \
     -v ${PWD}/config.json:/opt/omgservers/config.json:ro \
     -v /var/run/docker.sock:/var/run/docker.sock \
